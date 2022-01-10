d2e8ebd3312c2ea996ad3aedf46c6057e4e0a9a7
Mobirta-Tajima <97139391+Mobirta-Tajima@users.noreply.github.com>
2022-01-10 14:11:14 +0900

    training_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
-import React, { useState } from "react";
-import ColorfulMessage from "./components/ColorfulMessage";
+import React, { useEffect, useState } from "react";
+import { ColorfulMessage } from "./components/ColorfulMessage";
 
 const App = () => {
+  // console.log("ちゃす");
+  const [num, setNum] = useState(0);
+  const [FaceShowFlag, setFaceShowFlag] = useState(true);
+
   const onClickButton = () => {
     setNum(num + 1);
     // alert("ボタン押しました");
   };
-  const [num, setNum] = useState(0);
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!FaceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num % 5 === 0) {
+      FaceShowFlag || setFaceShowFlag(true);
+    } else {
+      FaceShowFlag && setFaceShowFlag(false);
+    }
+  }, [num]);
 
   return (
     <>
       <h1 style={{ color: "red" }}>イェーイ</h1>
       <ColorfulMessage color="blue">元気ですか？</ColorfulMessage>
       <ColorfulMessage color="red">元気です!</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickButton}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {FaceShowFlag && <p>（　＾ω＾）</p>}
     </>
   );
 };
